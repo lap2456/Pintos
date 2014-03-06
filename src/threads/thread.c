0671a7fd1babbe97c07a724e93795e3dbfd6a2d5
Louis Pujol <lap2456@cs.utexas.edu>
2014-03-06 14:10:45 -0600

    Morgan likes these

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -15,7 +15,7 @@
 #include "userprog/process.h"
 #endif
 
-//LOLLIPOPSSSSSS
+//Morgan loves LOLLIPOPSSSSSS
 
 /* Random value for struct thread's `magic' member.
    Used to detect stack overflow.  See the big comment at the top
