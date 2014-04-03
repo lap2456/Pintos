e971d14a6d24c15550daed808e11496663757747
Morgan Edgerton <morgan93@cs.utexas.edu>
2014-04-03 15:17:24 -0500

    worked on verify_pointer() -still one error sorry

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,18 +3,44 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/malloc.h"
+#include "threads/palloc.h"
+#include "threads/vaddr.h"
+#include "filesys/file.h"
+#include "filesys/filesys.h"
+#include "userprog/process.h"
+#
 
 static void syscall_handler (struct intr_frame *);
+
+static struct lock file_sys_lock;//added
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  lock_init(&file_sys_lock); 
 }
 
 static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
+  //added
+  if(!verify_pointer()){ //add arg
+  	//free resources
+  	//terminate thread
+  	thread_exit();
+  } 
+  //dereference
+
   printf ("system call!\n");
   thread_exit ();
+}
+
+//added
+/*verifies if user address requested is in user space and is not mapped to NULL*/
+static bool
+verify_pointer( const void* addr){
+
+	return( addr < PHYS_BASE && pagedir_get_page(thread_current()->pagedir, addr) !=NULL);
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -2,5 +2,6 @@
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
+static bool verify_pointer( const void* addr);
 
 #endif /* userprog/syscall.h */
